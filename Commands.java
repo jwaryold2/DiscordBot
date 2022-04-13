61e05f593eb7f05ae333e5ef6c7bfb3a882d6768
jwaryold2 <71293360+jwaryold2@users.noreply.github.com>
2022-04-13 14:50:24 -0400

    Update Commands.java
    Added spam ping command and  a wait method

--- Commands.java
+++ Commands.java
@@ -1,28 +1,29 @@
 package Main;
 
 import java.awt.Color;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.net.URL;
-
-import javax.imageio.ImageIO;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.Guild.Ban;
+import net.dv8tion.jda.api.interactions.commands.Command;
+import net.dv8tion.jda.api.utils.data.DataObject;
+import net.dv8tion.jda.internal.JDAImpl;
 
 public class Commands extends ListenerAdapter {
-	public String prefix = "~";
+	public String prefix = ".";
 	private int totalCope = 0;
+	private int usage = 0;
+	private boolean wait = false;
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		String args = event.getMessage().getContentRaw();
-		
+		String[] args1 = event.getMessage().getContentRaw().split(" ");
+
 		if (args.equalsIgnoreCase(prefix + "embed")) {
 			EmbedBuilder embed = new EmbedBuilder();
 			embed.setTitle("Embed Title", "");
@@ -74,23 +75,69 @@ public class Commands extends ListenerAdapter {
 				event.getMessage().reply("Please mention only one role to remove.").queue();
 			}
 		}
-		if (args.equalsIgnoreCase("cope")) {
+		if (args.contains("cope")) {
 			String adminId = "162295333873778689";
+			String botId = "903516162988597260";
 			// if you dont want it to reply do
 			// event.getChannel().sendMessage("test").queue();
 			// Member member =
 			// event.getGuild().getMember(event.getMessage().getMentionedUsers().get(0));
-			if(!event.getAuthor().getId().equals(adminId)) {
-			event.getMessage().reply("L+bozo+ratio").queue();
-			event.getChannel().sendMessage("https://tenor.com/view/talk-to-em-angry-mad-fish-spongebob-gif-17712353")
-					.queue();
+			if (!event.getAuthor().getId().equals(adminId) && !event.getAuthor().getId().equals(botId)) {
+				event.getMessage().reply("L+bozo+ratio").queue();
+				event.getChannel()
+						.sendMessage("https://tenor.com/view/talk-to-em-angry-mad-fish-spongebob-gif-17712353").queue();
 			}
 			this.totalCope += 1;
 		}
 		if (args.equalsIgnoreCase(prefix + "cope#")) {
-			event.getMessage().reply("Total cope used in this server: "+this.totalCope).queue();
+			event.getMessage().reply("Total cope used in this server: " + this.totalCope).queue();
 		}
-
+		String codeword = "spam";
+		codeword.concat(" mal");
+		if ((args.contains(codeword) && event.getAuthor().getId().equals("162295333873778689"))) {
+			// String result = event.getChannel().get
+			int i = 0;
+			/*
+			 * while (i < 100 && !args.equals("stop")) {
+			 * //event.getChannel().sendMessage("<@!321467507905134592>").queue();
+			 * event.getChannel().sendMessage("<@!162295333873778689>").queue(); }
+			 */
+		}
+		if (args1[0].equals("spam") /* && event.getAuthor().getId().equals("162295333873778689")*/){
+			List<User> mentioned = event.getMessage().getMentionedUsers();
+			if(usage < 1) {
+				if (mentioned.size() == 1) {
+					String mentioned_1 = mentioned.get(0).getId();
+					int i = 0;
+					while (i < 20) {
+						// event.getChannel().sendMessage("<@!321467507905134592>").queue();
+						event.getChannel().sendMessage("<@!" + mentioned_1 + ">").queue();
+						i++;
+					}
+					event.getChannel().sendMessage("Spam has finished with a total for "+i+" messages.").queue();
+					this.usage=1;
+				}
+				if (mentioned.size()>1) {
+					event.getMessage().reply("Sorry please mention only 1 user at a time. Thank you.").queue();
+					this.usage = 1;
+				}
+				wait(120000, TimeUnit.SECONDS);
+			} else {
+				event.getChannel().sendMessage("Please wait 2 minutes before using the command. If you spam the command it will reset the time to wait each time. Please only try once.").queue();
+				wait(120000, TimeUnit.SECONDS);
+			}
+			
+		}
+		
+	
+		
 	}
-
+	private void wait(int timeoutMillis, TimeUnit seconds) {
+		int time = 0;
+		for (int i = 0; i<timeoutMillis;i++) {
+			time += 1;
+		}
+		this.usage=0;
+		this.wait= true;
+	}
 }
