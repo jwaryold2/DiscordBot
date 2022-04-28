03417ea1c99a9afdaf81227af0c66697e5ce2891
jwaryold2 <71293360+jwaryold2@users.noreply.github.com>
2022-04-27 23:38:32 -0400

    Add files via upload

---
+++ WeatherResponse.java
@@ -0,0 +1,6 @@
+package Main;
+
+public class WeatherResponse {
+	WeatherResult[] weather;
+	WeatherResult main;
+}
---
+++ WeatherResult.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class WeatherResult {
+	String main;
+	String description;
+	double temp;
+	double feels_like;
+	double temp_min;
+	double temp_max;
+}
